0baaeafae6572f3a76308f332b3ee105e6b9f2c3
Franck Kamenan <franck.kamenan@yahoo.com>
2020-09-22 16:33:53 +0200

    Van kleinste naar grootste.

--- BoxApp.java
+++ BoxApp.java
@@ -6,7 +6,7 @@ public class BoxApp {
 
     public static void main(String[] args) {
 
-        Box box1 = new Box(10, 10, 10);
+        Box box1 = new Box(100, 100, 100);
         Box box2 = new Box(30, 5, 8);
         Box box3 = new Box(8, 15, 11);
 
@@ -15,6 +15,6 @@ public class BoxApp {
         boxSet.add(box2);
         boxSet.add(box3);
 
-        boxSet.forEach(System.out::println);
+        boxSet.forEach(System.out::println); // Van kleinste naar grootste.
     }
 }
