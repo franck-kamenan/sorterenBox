f58f3cfb0b5495d532295d43f4e94554798b2f81
Franck Kamenan <franck.kamenan@yahoo.com>
2020-09-23 10:57:04 +0200

    sort(Comparator.naturalOrder())

--- BoxListApp.java
+++ BoxListApp.java
@@ -6,16 +6,24 @@ public class BoxListApp {
 
     public static void main(String[] args) {
 
-        List<Box2> boxList = new ArrayList<>();
-        boxList.add(new Box2(100, 100, 100));
+        //List<Box2> boxList = new ArrayList<>();
+        /*boxList.add(new Box2(100, 100, 100));
         boxList.add(new Box2(8, 15, 30));
-        boxList.add(new Box2(15, 3, 45));
+        boxList.add(new Box2(15, 3, 45))*/;
 
-        boxList.sort(new BoxComparator());
+        /*boxList.sort(new BoxComparator());
         boxList.forEach(System.out::println);
 
         System.out.println("-----with lambda-----");
         boxList.sort(((o1, o2) -> o1.getVolume() - o2.getVolume()));
+        boxList.forEach(System.out::println);*/
+
+        List<Box> boxList = new ArrayList<>();
+        boxList.add(new Box(100, 100, 100));
+        boxList.add(new Box(8, 15, 30));
+        boxList.add(new Box(15, 3, 45));
+
+        boxList.sort(Comparator.naturalOrder());
         boxList.forEach(System.out::println);
     }
 }
